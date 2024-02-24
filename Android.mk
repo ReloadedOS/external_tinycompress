AOSPA/android_external_tinycompress

Conditionally Revert "tinycompress: do not loop in the compress read"
This change isn't ready for audio HALs before 5.10 and
breaks compress audio record, such as in WhatsApp.

This reverts commit f6700a93634f3c219a40b1298d12cbbf5ebf1160.

Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>
Change-Id: Ia87f587db74cad31585cac5fb5e628f29d1ba109

Android.mk
@@ -6,6 +6,9 @@ LOCAL_CFLAGS := -Wno-macro-redefined -Wno-unused-function
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)), true)
 LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
 endif
+ifeq ($(TARGET_NO_LOOP_COMPRESS_READ), true)
+LOCAL_CFLAGS += -DNO_LOOP_COMPRESS_READ
+endif
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -43,6 +46,9 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_SRC_FILES := compress.c utils.c
 LOCAL_CFLAGS += -DUSE_VENDOR_EXTN
 LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
+ifeq ($(TARGET_NO_LOOP_COMPRESS_READ), true)
+LOCAL_CFLAGS += -DNO_LOOP_COMPRESS_READ
+endif
 LOCAL_MODULE := libtinycompress_vendor
 LOCAL_SHARED_LIBRARIES := libcutils libutils
 LOCAL_MODULE_TAGS := optional

compress_plugin.c
@@ -43,7 +43,9 @@
 #include <linux/ioctl.h>
 #include <sound/asound.h>
 #include "tinycompress/compress_plugin.h"
+#ifndef NO_LOOP_COMPRESS_READ
 #include "tinycompress/tinycompress.h"
+#endif
 #include "sound/compress_offload.h"
 #include "compress_ops.h"
 #include "snd_utils.h"
@@ -99,9 +101,11 @@ static int compress_plug_set_params(struct compress_plug_data *plug_data,
 	rc = plugin->ops->set_params(plugin, params);
 	if (!rc) {
 		plugin->state = COMPRESS_PLUG_STATE_SETUP;
+#ifndef NO_LOOP_COMPRESS_READ
 		if (plug_data->flags & COMPRESS_OUT)
 			plugin->state = COMPRESS_PLUG_STATE_PREPARED;
 	}
+#endif
 
 	return rc;
 }

ReloadedOS/external_tinycompress

Android.mk
@@ -6,6 +6,9 @@ LOCAL_CFLAGS := -Wno-macro-redefined -Wno-unused-function
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)), true)
 LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
 endif
+ifeq ($(TARGET_LOOP_COMPRESS_READ), true)
+LOCAL_CFLAGS += -DLOOP_COMPRESS_READ
+endif
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -43,6 +46,9 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_SRC_FILES := compress.c utils.c
 LOCAL_CFLAGS += -DUSE_VENDOR_EXTN
 LOCAL_CFLAGS += -DENABLE_EXTENDED_COMPRESS_FORMAT
+ifeq ($(TARGET_LOOP_COMPRESS_READ), true)
+LOCAL_CFLAGS += -DLOOP_COMPRESS_READ
+endif
 LOCAL_MODULE := libtinycompress_vendor
 LOCAL_SHARED_LIBRARIES := libcutils libutils
 LOCAL_MODULE_TAGS := optional

compress_plugin.c
@@ -43,7 +43,9 @@
 #include <linux/ioctl.h>
 #include <sound/asound.h>
 #include "tinycompress/compress_plugin.h"
+#ifndef LOOP_COMPRESS_READ
 #include "tinycompress/tinycompress.h"
+#endif
 #include "sound/compress_offload.h"
 #include "compress_ops.h"
 #include "snd_utils.h"
@@ -99,8 +101,10 @@ static int compress_plug_set_params(struct compress_plug_data *plug_data,
 	rc = plugin->ops->set_params(plugin, params);
 	if (!rc) {
 		plugin->state = COMPRESS_PLUG_STATE_SETUP;
+#ifndef LOOP_COMPRESS_READ
 		if (plug_data->flags & COMPRESS_OUT)
 			plugin->state = COMPRESS_PLUG_STATE_PREPARED;
+#endif
 	}
 
 	return rc;